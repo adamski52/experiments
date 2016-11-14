800b604e6327e413efede2b8317772b497c1a5e5
Adamski <JADAMSK8@ford.com>
2016-11-14 14:48:19 -0500

    tile honeycomb.

--- tile-grid/app/config/config.ts
+++ tile-grid/app/config/config.ts
@@ -5,6 +5,6 @@ export class CONFIG {
     public FPS: number = 33;
     public MAX_SKIP:number = 10;
     public MAP_HEIGHT:number = 5;
-    public MAP_WIDTH:number = 15;
-    public TILE_SIZE:number = 15;
+    public MAP_WIDTH:number = 5;
+    public TILE_SIZE:number = 100;
 }
--- tile-grid/app/map/tile.ts
+++ tile-grid/app/map/tile.ts
@@ -5,15 +5,34 @@ import {CONFIG} from "../config/config";
 export class Tile {
     private _shape:createjs.Shape;
 
+    // i dont have an answer to this quite yet.  pixels being rectangular or something?
+    // every 50px of tile size creates 7 pixels of y-offset.
+    private _offset:number;
+
     constructor(private _x:number, private _y:number, private _size:number) {
+        this._offset = Math.floor((this._size / 50))*7;
+
         this._shape = new createjs.Shape();
-        this._shape.graphics.beginFill("black").drawCircle(0, 0, this._size);
+
+        this._shape.graphics.setStrokeStyle(1);
+        this._shape.graphics.beginStroke("#cccccc");
+        this._shape.graphics.beginFill("#f0f0f0");
+        this._shape.graphics.drawPolyStar(0, 0, this._size, 6, 0, 0);
         this.setPosition(this._x, this._y);
     }
 
     public setPosition(x:number, y:number):void {
-        this._shape.x = (x * (this._size*2)) + this._size;
-        this._shape.y = (y * (this._size*2)) + this._size;
+        this._x = x;
+        this._y = y;
+
+        if(this._y % 2 === 0) {
+            this._shape.x = (this._x * this._size * 3) + this._size;;
+        }
+        else {
+            this._shape.x = (this._x * this._size * 3) + (this._size * 1.5) + this._size;
+        }
+
+        this._shape.y = (this._y * (this._size - this._offset)) + this._size - this._offset;
     }
 
     public getElement():createjs.Shape {
