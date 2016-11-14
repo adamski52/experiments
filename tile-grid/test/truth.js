2a78fc33abca0f6152b1f706932f517609f4e4ef
Adamski <JADAMSK8@ford.com>
2016-11-14 11:45:30 -0500

    first pass at testing framework.

--- tile-grid/test/truth.js
+++ tile-grid/test/truth.js
@@ -1,13 +1,6 @@
-describe('universal truths', function () {
-    it('should do math', function () {
-        expect(1 + 1).toEqual(2);
-        expect(5).toBeGreaterThan(4);
-    });
-    xit('should skip this', function () {
-        expect(4).toEqual(40);
-    });
-    it('cant believe it', function () {
-        expect(4).toBe(4);
+describe("tests", function () {
+    it("should work", function () {
+        expect(4).toEqual(4);
     });
 });
 //# sourceMappingURL=truth.js.map
