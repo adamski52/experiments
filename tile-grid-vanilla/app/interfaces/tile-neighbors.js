072475a54e0b423a8d1e6c1fd8cdae2116828ed2
Adamski <JADAMSK8@ford.com>
2016-11-16 11:45:54 -0500

    dropping angular.  don't need it for anything after all.

--- tile-grid-vanilla/app/interfaces/tile-neighbors.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=tile-neighbors.js.map
--- tile-grid-vanilla/app/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var app_1 = require("./app");
-var app = new app_1.App();
-app.start();
-//# sourceMappingURL=main.js.map
